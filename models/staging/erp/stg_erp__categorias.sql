ff9b26b4b38a43c9ad445a45ddec17a5a88cb756
Matheus Vieira Perpetua <Matheusperpetua@hotmail.com>
2024-04-26 00:05:10 +0000

    Stages_Finalizadas

--- models/staging/erp/stg_erp__categorias.sql
+++ models/staging/erp/stg_erp__categorias.sql
@@ -1,11 +1,10 @@
 with
     fonte_categorias as (
         select
-            cast(id AS int) as pk_categoria
-            cast(categoryname as varchar)as nm_categoria
-            cast(description as varchar) as descricao_categoria
+            cast(ID as int) as pk_categoria
+            , cast(CATEGORYNAME as varchar) as nm_categoria
+            , cast(DESCRIPTION as varchar) as descricao_categoria
         from {{ source('erp', 'category') }}
     )
-
-select*
+select *
 from fonte_categorias
